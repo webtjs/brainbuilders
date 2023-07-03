1a3add8a8c83eb552786f44db91ecd4a27f5c97d
Xia Cong <xjqiao@outlook.co.nz>
2023-07-03 23:47:05 +0800

    greeting update

--- src/app/main-page/Banner.jsx
+++ src/app/main-page/Banner.jsx
@@ -3,10 +3,20 @@
 import { useState } from "react";
 import { AppBar, Toolbar } from "@mui/material";
 
+//Import from firebase
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+
 export default function Banner() {
-  const nameState = useState("<Anonymous user>");
-  const name = nameState[0];
-  const setName = nameState[1];
+  const [name, setName]  = useState("anonymous user") ;
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    setName(user.email);
+  }else {
+    //nothing happens
+  }
+})
 
   return (
     <header>
